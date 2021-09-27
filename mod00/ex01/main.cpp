e5aabaf0596c44b83d33f414b02d379c4e6ba0e5
yssefnoam <2020noam@gmail.com>
2021-09-27 16:48:07 +0100

    create the struct of the contact and the output of the command

--- mod00/ex01/main.cpp
+++ mod00/ex01/main.cpp
@@ -1,4 +1,34 @@
+#include <iostream>
+
+struct	contact{
+	std::string firstName;
+	std::string lastName
+	std::string nickName;
+	std::string phoneNumber;
+	std::string darkestSecret;
+}
+class	Head{
+};
+
 int main(void)
 {
+	std::string command;
+
+	while (1)
+	{
+		std::cout << "Enter a command (ADD,SEARCH,EXIT): ";
+		std::cin >> command;
+		if (command == "ADD") {
+
+		}
+		else if (command == "SEARCH") {
+
+		}
+		else if (command == "EXIT") {
+			std::cout << "Bye.\n";
+			return 0;
+		}
+
+	}
 	return 0;
 }
