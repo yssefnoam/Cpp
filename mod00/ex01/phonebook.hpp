ffcdd3434582fbd9a455e6336228297e33961f74
yssefnoam <2020noam@gmail.com>
2021-09-27 18:16:43 +0100

    try to find the struct of the data chould i use

--- mod00/ex01/phonebook.hpp
+++ mod00/ex01/phonebook.hpp
@@ -3,14 +3,22 @@
 #include <iostream>
 
 struct	Contact{
-	std::string firstName;
-	std::string lastName;
-	std::string nickName;
-	std::string phoneNumber;
-	std::string darkestSecret;
+	std::string firstName = std::string(NULL);
+	std::string lastName = std::string(NULL);
+	std::string nickName = std::string(NULL);
+	std::string phoneNumber = std::string(NULL);
+	std::string darkestSecret = std::string(NULL);
 };
 
 class	PhoneBook{
+
+	public:
+	PhoneBook(void){
+		struct Contact contactList[8];
+
+	}
+	void addContact(void);
+
 };
 
 #endif
