7ad4a3860625e0ceae67ee4d216f038c44e5334b
yssefnoam <2020noam@gmail.com>
2021-10-10 19:16:16 +0100

    try to solv the complain function

---
+++ mod01/ex05/Karen.hpp
@@ -0,0 +1,16 @@
+#ifndef KAREN_HPP
+#define KAREN_HPP
+
+class Karen
+{
+private:
+    void debug(void);
+    void info(void);
+    void warning(void);
+    void error(void);
+
+public:
+    void complain(std::string level);
+};
+
+#endif
---
+++ mod01/ex05/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Karen.hpp"
+
+int main(void)
+{
+    Karen karen;
+
+    karen.complain("ERROR");
+
+    return 0;
+}
