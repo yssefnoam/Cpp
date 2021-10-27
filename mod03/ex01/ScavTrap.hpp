10d8675d62f893181983f97d365ddd5b4127cb58
yssefnoam <2020noam@gmail.com>
2021-10-27 18:51:09 +0100

    a

---
+++ mod03/ex01/ScavTrap.hpp
@@ -0,0 +1,16 @@
+#ifndef _SCAV_TRAP_HPP
+#define _SCAV_TRAP_HPP
+#include <iostream>
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+public:
+    ScavTrap();
+    ~ScavTrap();
+    ScavTrap(const ScavTrap&);
+    ScavTrap& operator=(const ScavTrap&);
+    void    attack(std::string const &);
+    void    guardGate();
+};
+#endif
