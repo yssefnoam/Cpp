e46d3fc91b4bd3913850d1ade0f0938683584d03
yssefnoam <2020noam@gmail.com>
2021-11-20 17:52:52 +0100

    finish

--- mod06/ex02/main.cpp
+++ mod06/ex02/main.cpp
@@ -4,19 +4,75 @@
 #include "A.hpp"
 #include "C.hpp"
 
-Base* generate()
+Base *generate()
 {
+	std::srand(std::time(NULL));
+	switch (std::rand() % 3)
+	{
+	case 0:
+		return new A();
+	case 1:
+		return new B();
+	case 2:
+		return new C();
+	}
+	return NULL;
 }
 
 void identify(Base *p)
 {
+	if (dynamic_cast<A *>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B *>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C *>(p))
+		std::cout << "C" << std::endl;
 }
 
 void identify(Base &p)
 {
+	try
+	{
+		Base &tmp = dynamic_cast<A &>(p);
+		static_cast<void>(tmp);
+		std::cout << "A" << std::endl;
+		return;
+	}
+	catch (...)
+	{
+	}
+	try
+	{
+		Base &tmp = dynamic_cast<B &>(p);
+		static_cast<void>(tmp);
+		std::cout << "B" << std::endl;
+		return;
+	}
+	catch (...)
+	{
+	}
+	try
+	{
+		Base &tmp = dynamic_cast<C &>(p);
+		static_cast<void>(tmp);
+		std::cout << "C" << std::endl;
+		return;
+	}
+	catch (...)
+	{
+	}
 }
 
 int main(void)
 {
+	identify(generate());
+
+	Base &a = A();
+	identify(a);
+	B &b = B();
+	identify(b);
+	C &c = C();
+	identify(c);
+
 	return 0;
 }
