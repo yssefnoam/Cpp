ce55d009034b6ed95bcc2970b949f0654c9d7232
yssefnoam <2020noam@gmail.com>
2021-11-22 04:46:34 +0100

    start mod07

---
+++ mod07/ex01/iter.hpp
@@ -0,0 +1,14 @@
+#ifndef ITER_HPP
+#define ITER_HPP
+
+template<typename Add, size_t length, typename Fun>
+void    iter(Add *address, size_t length, Fun ft)
+{
+    for (size_t i = 0; i < length; i++)
+    {
+        /* code */
+    }
+    
+}
+
+#endif
