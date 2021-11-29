62af24c2a0a46284e8580a80a2a4abe59016e39c
Youssef Noam <ynoam@e1r13p9.1337.ma>
2021-11-29 02:49:07 +0100

    a

--- mod08/ex02/main.cpp
+++ mod08/ex02/main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 #include "mutantstack.hpp"
 
-int main(void)
+int main()
 {
     MutantStack<int> mstack;
-    mstack.push(1);
-    std::cout << mstack.top() << std::endl;
-    mstack.push(2);
+    mstack.push(5);
+    mstack.push(17);
     std::cout << mstack.top() << std::endl;
     mstack.pop();
-    std::cout << mstack.top() << std::endl;
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737); //[...]
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
     return 0;
 }
 
-// clang++ -Wall -Wextra -Werror main.cpp && ./a.out
+    // clang++ -Wall -Wextra -Werror main.cpp && ./a.out
--- mod08/ex02/mutantstack.cpp
+++ mod08/ex02/mutantstack.cpp
@@ -2,13 +2,25 @@
 #include "mutantstack.hpp"
 
 template<typename T>
-typename MutantStack<T>::iterator MutantStack<T>::begin() { return NULL; }
+typename MutantStack<T>::iterator MutantStack<T>::begin()
+{
+    return std::stack<T>::container_type::begin();
+}
 
 template<typename T>
-typename MutantStack<T>::iterator MutantStack<T>::end() { return NULL; }
+typename MutantStack<T>::iterator MutantStack<T>::end()
+{
+    return std::stack<T>::container_type::end();
+}
 
 template<typename T>
-typename MutantStack<T>::iterator MutantStack<T>::rbegin() { return NULL; }
+typename MutantStack<T>::iterator MutantStack<T>::rbegin()
+{
+    return std::stack<T>::container_type::rbegin();
+}
 
 template<typename T>
-typename MutantStack<T>::iterator MutantStack<T>::rend() { return NULL; }
+typename MutantStack<T>::iterator MutantStack<T>::rend()
+{
+    return std::stack<T>::container_type::rend();
+}
