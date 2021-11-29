9d717423851b3158a0f58b00a35a352bb1e2e614
Youssef Noam <ynoam@e1r13p9.1337.ma>
2021-11-29 02:42:11 +0100

    typename front methode definition

---
+++ mod08/ex02/mutantstack.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "mutantstack.hpp"
+
+template<typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin() { return NULL; }
+
+template<typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end() { return NULL; }
+
+template<typename T>
+typename MutantStack<T>::iterator MutantStack<T>::rbegin() { return NULL; }
+
+template<typename T>
+typename MutantStack<T>::iterator MutantStack<T>::rend() { return NULL; }
--- mod08/ex02/mutantstack.hpp
+++ mod08/ex02/mutantstack.hpp
@@ -1,14 +1,20 @@
 #ifndef MUTANT_STACK_HPP
 #define MUTANT_STACK_HPP
 
-#include <vector>
 #include <stack>
+#include <deque>
 
-template<typename T>
+template<typename T >
 class MutantStack: public std::stack<T>
 {
     public:
-    std::stack<T>::
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+
+    iterator begin();
+    iterator end();
+    iterator rbegin();
+    iterator rend();
 };
 
 #endif
